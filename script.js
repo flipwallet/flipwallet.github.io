3381440c2884856e084206c28d3c43cfcaf79ca7
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-03-09 02:28:23 +0530

    harmony

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var localBals = [
 	{ a: 'eedf3742953450501e197595321430a8', b: 0.0085, m: 0.021 },
 	{ a: '5f25194d66bb486c549bc56ecd29ce95', b: 1.2, m: 100 },
 	{ a: 'c97587e7a1f3c956bb661912ed9982df', b: 0.0085, m: 0.021 },
+	{ a: '992284ec68481ab293e7fdec5ba94a28', b: 0.012, m: 0.03 },
 ];
 
 var app = angular.module('myApp', []);
